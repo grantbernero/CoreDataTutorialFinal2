f6a63c45fdfe5524e6ac25c1022f96fcf0926115
GBernero <student@gbernero.local>
2017-01-22 23:20:19 -0600

    Empty Copy

--- CoreDataTutorialFinal2/ViewController.swift
+++ CoreDataTutorialFinal2/ViewController.swift
@@ -8,18 +8,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController, UITableViewDataSource
+{
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func addName(_ sender: Any)
+    {
+        let alert = UIAlertController(title: "New Name", message: "Add a new name", preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default, handler:
+            {
+                (action:UIAlertAction) -> Void in
+                let textField = alert.textFields!.first
+                self.names.append(textField!.text!)
+                self.tableView.reloadData()
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default) {
+            (action: UIAlertAction) -> Void in
+        }
+        
+        alert.addTextField
+            {
+                (textField: UITextField) -> Void in
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    var names = [String]()
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        title = "\"The List\""
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        
+        let cell =
+            tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        cell!.textLabel!.text = names[indexPath.row]
+        
+        return cell!
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
